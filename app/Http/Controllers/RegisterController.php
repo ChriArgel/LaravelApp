2e4d3dafcda07acb23d61e216add3f7050789cf5
ChriArgel <chrisarmlae@gmail.com>
2023-01-19 20:17:20 -0600

    cambios en elcontrolador register y en las migraciones agregando username

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -18,8 +20,15 @@ class RegisterController extends Controller
             'name' => 'required|min:5',
             'username' => 'required|unique:users|min:3|max:30',
             'email' => 'required|unique:users|email',
-            'password' => 'required',
+            'password' => 'required|confirmed|min:6',
 
+        ]);
+
+        User::create([
+            'name' => $request -> name,
+            'username' => $request -> username,
+            'email' => $request -> email,
+            'password' => Hash::make($request -> password)
         ]);
 
 
